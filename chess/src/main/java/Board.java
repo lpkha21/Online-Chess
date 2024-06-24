2fb97692ffe079f9a5ae515ef2fe305f8d37f563
luka <lpkha21@freeuni.edu.ge>
2024-06-24 14:01:05 +0400

    changed Board.java

--- chess/src/main/java/Board.java
+++ chess/src/main/java/Board.java
@@ -2,9 +2,14 @@ import java.util.ArrayList;
 
 public class Board {
     static final int SIZE = 8;
-    ArrayList<ArrayList<Piece>> board;
+    Piece[][] board;
     //dasaweria bordis sheqmna 8X8 nalebit shevseba da 32 figuris sheqmna da ganlageba
     public Board(){
+        board = new Piece[8][8];
 
+    }
+
+    public boolean isEmpty(int i, int j){
+        return board[i][j] == null;
     }
 }
--- chess/src/main/java/Queen.java
+++ chess/src/main/java/Queen.java
@@ -1,2 +1,30 @@
-public class Queen {
+public class Queen implements Piece{
+    int color;
+
+    final int type = pieceEnum.QUEEN;
+
+    Coordinate cord;
+
+    Board board;
+
+    @Override
+    public boolean canMove(Coordinate c) {
+
+        return false;
+    }
+
+    @Override
+    public int color() {
+        return color;
+    }
+
+    @Override
+    public int getType() {
+        return type;
+    }
+
+    @Override
+    public Coordinate getCoordinate() {
+        return cord;
+    }
 }
