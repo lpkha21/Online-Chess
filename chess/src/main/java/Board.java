086d7c52685a5b2a8811d0819e5656f23119c861
luka pkhaladze <lpkha21@freeuni.edu.ge>
2024-06-24 17:11:47 +0400

    added Board getPiece

--- chess/src/main/java/Board.java
+++ chess/src/main/java/Board.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Board {
     static final int SIZE = 8;
-    Piece[][] board;
+    private final Piece[][] board;
     //dasaweria bordis sheqmna 8X8 nalebit shevseba da 32 figuris sheqmna da ganlageba
     public Board(){
         board = new Piece[SIZE][SIZE];
@@ -15,4 +15,12 @@ public class Board {
     public boolean isEmpty(int i, int j){
         return board[i][j] == null;
     }
+    public Piece getPiece(Coordinate cord){
+        return board[cord.i][cord.j];
+    }
+
+    public Piece getPiece(int i, int j){
+        return board[i][j];
+    }
+
 }
