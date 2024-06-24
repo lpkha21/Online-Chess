7512fa347b7c4cf67a38634b0ab1a4992f67253e
Nikoloz Kharshiladze <nkhar21@freeuni.edu.ge>
2024-06-24 18:01:15 +0400

    add bishop 2.0

--- chess/src/main/java/Bishop.java
+++ chess/src/main/java/Bishop.java
@@ -13,22 +13,52 @@ public class Bishop implements Piece{
     }
     @Override
     public boolean canMove(Coordinate c) {
-        int dx = this.coord.j - c.j;
-        int dy = this.coord.i - c.i;
+        int dx = c.j - this.coord.j;
+        int dy = c.i - this.coord.i;
 
-        if( dx == 0 && dy == 0 ){  // on the same square
+        if( dx == 0 && dy == 0 ) // move to same block
             return false;
-        } else if ( abs(dx) != abs(dy) ) { // not on the diagonal
-            return false;
-        } else if( !board.isEmpty(c.i,c.j) && board.getPiece(c.i, c.j).color() == this.color ){
-            return false;
-        }
 
-        // check the path to c
-        for(int row=this.coord.i; row<this.coord.i+dx; row++) {
-            for(int col=this.coord.j; col<this.coord.j+dy; col++) {
-                if ( !board.isEmpty(row,col) ) {
-                    return false;
+        if ( abs(dx) != abs(dy) ) // move is not diagonal
+            return false;
+
+        // moving to a same colored piece
+        if( !board.isEmpty(c.i,c.j) && board.getPiece(c.i, c.j).color() == this.color )
+            return false;
+
+
+        // check the paths to c
+
+        if(dx > 0 && dy > 0) { // 1. bottom right
+            for (int row = this.coord.i; row < this.coord.i + dy; row++) {
+                for (int col = this.coord.j; col < this.coord.j + dx; col++) {
+                    if (!board.isEmpty(row, col)) {
+                        return false;
+                    }
+                }
+            }
+        } else if(dx > 0 && dy < 0) { // 2. top right
+            for (int row = this.coord.i; row > this.coord.i + dy; row--) {
+                for (int col = this.coord.j; col < this.coord.j + dx; col++) {
+                    if (!board.isEmpty(row, col)) {
+                        return false;
+                    }
+                }
+            }
+        } else if(dx < 0 && dy > 0) { // 3. bottom left
+            for (int row = this.coord.i; row < this.coord.i + dy; row++) {
+                for (int col = this.coord.j; col > this.coord.j + dx; col--) {
+                    if (!board.isEmpty(row, col)) {
+                        return false;
+                    }
+                }
+            }
+        } else { // 4. top left
+            for (int row = this.coord.i; row > this.coord.i + dy; row--) {
+                for (int col = this.coord.j; col > this.coord.j + dx; col--) {
+                    if (!board.isEmpty(row, col)) {
+                        return false;
+                    }
                 }
             }
         }
