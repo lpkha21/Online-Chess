bd5cfb7b810269537e62750a2f7e8b593f35840e
Irakli Shelia <ishel21@freeuni.edu.ge>
2024-06-24 13:53:25 +0400

    v1.2

--- chess/src/main/java/King.java
+++ chess/src/main/java/King.java
@@ -1,2 +1,32 @@
-public class King {
+public class King implements Piece{
+    int color;
+    final int type = pieceEnum.KING;
+    Coordinate coord;
+    Board board;
+
+
+    public King(Coordinate coord, Board board, int color){
+        this.coord = coord;
+        this.board = board;
+    }
+
+    @Override
+    public boolean canMove(Coordinate c) {
+        return false;
+    }
+
+    @Override
+    public int color() {
+        return color;
+    }
+
+    @Override
+    public int getType() {
+        return type;
+    }
+
+    @Override
+    public Coordinate getCoordinate() {
+        return coord;
+    }
 }
--- chess/src/main/java/Rook.java
+++ chess/src/main/java/Rook.java
@@ -1,2 +1,6 @@
 public class Rook {
+    int color;
+    final int type = pieceEnum.KING;
+    Coordinate coord;
+    Board board;
 }
